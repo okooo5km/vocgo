4fd9cb7f436cdbcaf8adcdab0595dd433d9c2d1a
5km <5km@smslit.cn>
2021-04-07 20:50:27 +0800

    update version

--- vocgo/consts.py
+++ vocgo/consts.py
@@ -8,5 +8,5 @@
 @contact       : 5km@smslit.cn
 """
 
-__version__ = "0.1.0-alpha"
+__version__ = "0.1.0"
 __description__ = "This is a simple tool for VOC dataset, it can help you analyze and process the dataset, its chinese name is 「窝酷狗」，more details to https://github.com/smslit/vocgo"
